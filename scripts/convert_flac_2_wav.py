936bdf6826bc62330e4afa8b584e672ce9a58633
uday160386 <vuktales@gmail.com>
2024-08-18 17:42:43 +0800

    ms is ready for file upload

--- scripts/convert_flac_2_wav.py
+++ scripts/convert_flac_2_wav.py
@@ -3,6 +3,10 @@ import shutil
 
 import glob
 
+from pathlib import PurePath
+from pydub import AudioSegment
+
+
 mod_root = "test_audio_files/"
 FLAC_HOME_FILES = mod_root+"LibriSpeech/test-clean/"
 TRAIN_DATA_HOME =  mod_root+"wav"
@@ -17,8 +21,9 @@ def converting_wav_files():
                     os.makedirs(TRAIN_DATA_HOME+"/"+get_sub_dir[0]+"/"+ get_sub_dir[1])
 
                 # converting data to .wav files
-                shutil.copyfile(FLAC_HOME_FILES+get_sub_dir[0]+'/'+get_sub_dir[1]+'/'+name,
-                TRAIN_DATA_HOME+"/"+get_sub_dir[0]+'/'+get_sub_dir[1]+'/'+split_filename[0]+'.wav' ) 
+                file_path = PurePath(FLAC_HOME_FILES+get_sub_dir[0]+'/'+get_sub_dir[1]+'/'+name)
+                flac_tmp_audio_data = AudioSegment.from_file(file_path, file_path.suffix[1:])
+                flac_tmp_audio_data.export(TRAIN_DATA_HOME+"/"+get_sub_dir[0]+'/'+get_sub_dir[1]+'/'+split_filename[0] + ".wav", format="wav")
 
     print('Files converted to WAV sucussfully.....')
 
