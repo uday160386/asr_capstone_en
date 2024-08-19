242ef66d5938c21c504e2355760d89e8c124ef06
uday160386 <vuktales@gmail.com>
2024-08-19 15:32:09 +0800

    add ms

--- main.py
+++ main.py
@@ -11,32 +11,10 @@ app = FastAPI()
 from typing import Annotated
 
 from fastapi import FastAPI, File, Form
-
-mod_root = "test_audio_files/"
-TRAIN_DATA_HOME =  mod_root+"tmp/"
-
-@app.post("/api/audio-file")
-async def post_media_file(uploaded_file: str):
-    """
-    Receive File, store to disk & return it
-    """
-    # Write file to disk. This simulates some business logic that results in a file sotred on disk
     
-@app.post("/upload/")
+@app.post("/api/audio-file")
 async def upload_file(
-    audio_bytes: Annotated[bytes, File()],
-    file_name: Annotated[str, Form()],
+    audio_file_path: Annotated[str, Form()],
 ):
-    if len(audio_bytes) == 0:
-       return {"Error":"Please send valid data for audio_bytes"}
-    if len(file_name) == 0:
-       return {"Error":"Please enter value for filename"}
-    with wave.open(TRAIN_DATA_HOME+file_name+'.wav', 'wb') as wav_file: 
-      wav_file.setnchannels(1) # Mono
-      wav_file.setsampwidth(2) # Sample width in bytes
-      wav_file.setnframes(78480)
-      wav_file.setframerate(16000) # Sample rate
-      wav_file.writeframes(audio_bytes)
-
-    asr_out=execute_asr_cap_en.calling_asr(TRAIN_DATA_HOME+file_name+'.wav',"en_ID")
+    asr_out=execute_asr_cap_en.calling_asr(audio_file_path,"en_ID")
     return {"out-put-string": asr_out}
--- scripts/read_wav_file.py
+++ scripts/read_wav_file.py
@@ -10,16 +10,17 @@ from  scipy.io import wavfile
 
 import wave
 # Open a WAV file for reading.
-with wave.open("/Users/udaykumar/Documents/workspace/asr_capstone_en/test_audio_files/wav/121/121726/121-121726-0000.wav", 'rb') as wav_file:
+with wave.open("/Users/udaykumar/Documents/workspace/asr_capstone_en/test_audio_files/wav/61/70968/61-70968-0000.wav", 'rb') as wav_file:
     # Read audio frames and get parameters.
     audio_frames = wav_file.readframes(wav_file.getnframes())
     params = wav_file.getparams()
     print(audio_frames)
-# # Open a new WAV file for writing and set parameters.
-# with wave.open("/Users/udaykumar/Documents/workspace/asr_capstone_en/test_audio_files/tmp/61-70968-0000.wav", 'wb') as wav_file:
-#     wav_file.setparams(params)
-#     # Write audio frames to the new WAV file.
-#     wav_file.writeframes(audio_frames)
+# Open a new WAV file for writing and set parameters.
+
+with wave.open("/Users/udaykumar/Documents/workspace/asr_capstone_en/test_audio_files/tmp/61-70968-0000.wav", 'wb') as wav_file:
+    wav_file.setparams(params)
+    # Write audio frames to the new WAV file.
+    wav_file.writeframes(audio_frames)
 
 
 # with wave.open("/Users/udaykumar/Documents/workspace/asr_capstone_en/test_audio_files/wav/61/70968/61-70968-0000.wav", 'rb') as wav_file: 
